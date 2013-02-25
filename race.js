3ee581fa43912f3c836b717affb7494d4bbc8628
Michael Chary <mac389@gmail.com>
2013-02-25 16:52:17 -0500

    Deleted more files
    Smultron project files aren't wise to include because they are very
    platform-specific

--- race.js
+++
@@ -1,5 +0,0 @@
-<script type="text/javascript" src="/ajax/ageV1W1EJ.js"></script>
-<jsxgraph width="500" height="500">
-brd = JXG.JSXGraph.initBoard('jxgbox', {boundingbox:[-850,110,850,-20],axis:false});
-var slide = brd.create('slider',[[-700,-5],[300,-5],[2009,2009,2060]],{name:'year',snapWidth:1});
- 
